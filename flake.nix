c3880ff7aea7ab99e1c410367ce9c74e32a91502
RGBCube <RGBCube@users.noreply.github.com>
2023-11-27 15:58:26 +0300

    Fix

--- flake.nix
+++ flake.nix
@@ -7,11 +7,13 @@
 
       with0x      = theme // (builtins.mapAttrs (_: value: "0x" + value) onlyColors);
       withHashtag = theme // (builtins.mapAttrs (_: value: "#" + value)  onlyColors);
-    in theme // {
-      inherit with0x withHashtag;
 
-      tmTheme       = (import ./templates/tmTheme.nix) theme;
-      adwaitaGtkcss = (import ./templates/adwaitaGtkCss.nix) theme;
+      themeFull = theme // {
+        inherit with0x withHashtag;
+      };
+    in themeFull // {
+      tmTheme       = (import ./templates/tmTheme.nix) themeFull;
+      adwaitaGtkcss = (import ./templates/adwaitaGtkCss.nix) themeFull;
     };
-  } // builtins.mapAttrs (_: self.custom) (import ./themes.nix);
+  } // builtins.mapAttrs (name: self.custom) (import ./themes.nix);
 }
