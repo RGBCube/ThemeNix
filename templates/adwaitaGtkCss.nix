edf1d71cb1b16a32fb10b91bbc74afec82de7cf0
RGBCube <RGBCube@users.noreply.github.com>
2023-12-01 15:01:32 +0300

    Add Adwaita GTK CSS instruction

--- templates/adwaitaGtkCss.nix
+++ templates/adwaitaGtkCss.nix
@@ -1,3 +1,9 @@
+# With Home Manager, do:
+#
+# xdg.configFile = {
+#   "gtk-3.0/gtk.css".text = myTheme.adwaitaGtkCss;
+#   "gtk-4.0/gtk.css".text = myTheme.adwaitaGtkCss;
+# };
 theme: with theme.withHashtag; ''
 @define-color accent_color ${base0A};
 @define-color accent_bg_color ${base0A};
